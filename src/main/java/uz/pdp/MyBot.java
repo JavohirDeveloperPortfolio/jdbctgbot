b09ebd957d5f127a36d19657b2f89510cf9e837f
Javohir <hjavohirir@gmail.com>
2024-04-23 10:38:28 +0500

    Update Language qo'shildi

--- src/main/java/uz/pdp/MyBot.java
+++ src/main/java/uz/pdp/MyBot.java
@@ -1,12 +1,73 @@
 package uz.pdp;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import uz.pdp.model.BotState;
+import uz.pdp.model.User;
+import uz.pdp.model.UserRepository;
+
+import java.util.List;
 
 public class MyBot extends TelegramLongPollingBot {
+    private final UserRepository userRepository =
+            new UserRepository();
+
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()){
+            String chatId = update.getMessage().getChatId().toString();
+            String text = update.getMessage().getText();
 
+            if (text.equals("/start")){
+                if (userRepository.getUserByChatId(chatId) == null){
+                    userRepository.insertUser(chatId, BotState.MAIN_PAGE);
+                }
+                SendMessage message = new SendMessage();
+                message.setText("Xush kelibsiz. Tilni tanlang");
+                message.setChatId(chatId);
+
+                ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup();
+                KeyboardRow row1 = new KeyboardRow(List.of(new KeyboardButton("O'zbek tili")));
+                KeyboardRow row2 = new KeyboardRow(List.of(new KeyboardButton("Ro's tili")));
+                markup.setKeyboard(List.of(row1,row2));
+                message.setReplyMarkup(markup);
+
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                User user = userRepository.getUserByChatId(chatId);
+                if (user.getBotState() == BotState.MAIN_PAGE){
+                    if (text.equals("O'zbek tili")){
+                        userRepository.updateLang(chatId,"uz");
+                        SendMessage message = new SendMessage();
+                        message.setText("O'zbek tili tanlandi");
+                        message.setChatId(chatId);
+                        try {
+                            execute(message);
+                        } catch (TelegramApiException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else if (text.equals("Ro's tili")) {
+                        SendMessage message = new SendMessage();
+                        message.setText("Ro's tili tanlandi");
+                        message.setChatId(chatId);
+                        try {
+                            execute(message);
+                        } catch (TelegramApiException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Override
--- src/main/java/uz/pdp/model/UserRepository.java
+++ src/main/java/uz/pdp/model/UserRepository.java
@@ -12,14 +12,29 @@ public class UserRepository {
             "INSERT INTO botusers (chat_id,bot_state) VALUES (?,?)";
     private final String selectUserByChatId =
             "SELECT * FROM botusers WHERE chat_id = ?";
+    private final String updateLang =
+            "UPDATE botusers SET lang = ? WHERE chat_id = ?";
 
-    public void insertUser(String chatId, BotState botState){
+    public void updateLang(String chatId, String lang){
+        try{
+            Connection conn = CustomDataSource.getInstance().getConnection();
+            PreparedStatement ps = conn.prepareStatement(updateLang);
+
+            ps.setString(1,lang);
+            ps.setString(2,chatId);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insertUser(String chatId, BotState botState) {
         try {
             Connection conn = CustomDataSource.getInstance().getConnection();
             PreparedStatement ps = conn.prepareStatement(insertUser);
 
-            ps.setString(1,chatId);
-            ps.setString(2,botState.name());
+            ps.setString(1, chatId);
+            ps.setString(2, botState.name());
 
             ps.executeUpdate();
         } catch (SQLException e) {
@@ -27,20 +42,23 @@ public class UserRepository {
         }
     }
 
-    public User getUserByChatId(String chatId){
+
+    public User getUserByChatId(String chatId) {
         User user = new User();
         try {
             Connection conn = CustomDataSource.getInstance().getConnection();
             PreparedStatement ps = conn.prepareStatement(selectUserByChatId);
 
-            ps.setString(1,chatId);
+            ps.setString(1, chatId);
 
             ResultSet resultSet = ps.executeQuery();
-            while (resultSet.next()){
-                user.setBotState(
-                        BotState.valueOf(resultSet.getString("bot_state")));
-                user.setChatId(resultSet.getString("chat_id"));
+            if (!resultSet.next()) {
+                return null;
             }
+            user.setBotState(
+                    BotState.valueOf(resultSet.getString("bot_state")));
+            user.setChatId(resultSet.getString("chat_id"));
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
